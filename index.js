c4cbb323efa92628b6dfab23137516f029aecf9e
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-07 02:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   // deletes key from clone of object
   // returns new object
-  var recipes = { ingredients: 'eggs' }
+  var newObj = Object.assign({}, object)
 
-  var newObj = Object.assign({}, recipes)
-
-  newObj
 
   delete newObj.key
 
