8bfd2bbc1b585cfbc0f94b85daa2525d21b2969b
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-07 02:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
 
 var object = {}
-delete object.key
+delete [object].key
 return object
 
 }
