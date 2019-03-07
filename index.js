1b7a76e057823c10eef24a0a280225c8af5cdbef
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-07 01:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object; 
 
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
